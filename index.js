0b6879cf0c98bd4dc25fd6eb093308ea20751c5f
NicoleOrmerod94 <147579584+NicoleOrmerod94@users.noreply.github.com>
2023-10-25 11:00:13 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+  
